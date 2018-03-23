9c7910f6c258490a4a322266ebc5551311de45fd
Miguel Angel Juarez Hernandez <mjuarez@anzen.com.mx>
2018-03-23 17:26:45 -0600

    add test language

---
+++ src/main/java/com/mikel/language/TestLiterales.java
@@ -0,0 +1,36 @@
+package com.mikel.language;
+
+public class TestLiterales {
+
+
+    public static void main(String[] args) {
+
+        //Los numeros por default son asumidos por valor int  (decimal )
+
+
+        //OCTAL(0-7), usan el 0 como prefijo, ej
+        int uno = 01;
+        int dos = 02;
+        int siete = 07;
+        int ocho = 017;
+        double d_ocho = 019.92;// no es octal,se considera decimal
+
+        //EXADECIMAL ( 0-9 , A-F) usan prefijo 0x o 0X
+
+        byte exa_uno = 0X1;
+        int exa_once = 0xA;
+        float exa_quince = 0XF;
+        //double exa_double = 0xF.1; no se pueden usar exadecimales double
+        System.out.println("exa_dieciseis->"+exa_quince); //imprime en decimal
+
+        //BINARIO (0-1) usa prefijo 0b o OB
+        int bin_cero = 0b0;
+        int bin_uno = 0B1;
+        Integer bin_dos = 0b10;
+        double d_dos = 0b100101;
+        //double double_bin = 0b10.1;no se puede usar binario tipo double
+        System.out.println("bin_cero->"+bin_cero+" , bin_uno->"+bin_uno+" , bin_dos->"+bin_dos);
+
+
+    }
+}
---
+++ src/main/java/com/mikel/language/TestSwitch.java
@@ -0,0 +1,20 @@
+package com.mikel.language;
+
+public class TestSwitch {
+
+    public static void main(String[] args) {
+
+        /*
+        la sentencia switch soporta las siguientes tipos de datos
+            -int y Integer
+            -byte y Byte
+            -short y Short
+            -char y Character
+            -String
+            -valores enum
+        No soporta boolean y long
+        las sentencias dentro de “case” deben ser literales, constantes enum o variables constantes final
+         */
+
+    }
+}
